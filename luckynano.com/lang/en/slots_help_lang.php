4d45daa67d0b7082bf58ffb4452b960622ec8577
Anderson-Garcia <68165804+Anderson-Garcia@users.noreply.github.com>
2020-07-16 08:44:21 -0400

    Update slots_help_lang.php

--- luckynano.com/lang/en/slots_help_lang.php
+++ luckynano.com/lang/en/slots_help_lang.php
@@ -3,24 +3,24 @@ $slots_help_lang = array(
   'chart_column_6' => 
   	'<div class="chart_column" id="chart_column_6">
       <div class="chart_column_title">BONUS SYMBOLS</div>
-      <div class="chart_combo_bonus"><img src="images/golden/11_bell.png"/> replace any other symbol in a combo, except for SCATTER symbols.
+      <div class="chart_combo_bonus"><img src="images/golden/11_bell.png"/> toma el lugar de cualquier otro símbolo en el combo, exceptuando los de SCATTER.
       </div>
-      <div class="chart_combo_bonus"><img src="images/golden/10_strawberry.png"/> spread across the reels, and grant loads of <img class="gold_ticket_logo" src="images/assets/gold_ticket.svg"> ! *
+      <div class="chart_combo_bonus"><img src="images/golden/10_strawberry.png"/> que se encuentren, y te otorgan muchos <img class="gold_ticket_logo" src="images/assets/gold_ticket.svg"> ! *
       	<br>
-      	Golden Tickets <img class="gold_ticket_logo" src="images/assets/gold_ticket.svg"> are free games, each holding the value of your last bet.
+      	Los Golden Tickets <img class="gold_ticket_logo" src="images/assets/gold_ticket.svg"> son juegos gratuitos, cada uno con el valor de tu última apuesta.
       </div>
-      <div class="chart_combo_bonus" id="chart_combo_bonus_coming_soon">New bonuses to come in next updates.
+      <div class="chart_combo_bonus" id="chart_combo_bonus_coming_soon">En las próximas actualizaciones encontrarás nuevas bonificaciones.
       </div>
 
       <div class="chart_column_title">PAYLINES</div>
       <img id="golden_paylines" src="images/golden/info_1_01.png"/>
     </div>',
   'golden_info_bottom_line' => 
-  	'Gains are multiplied by the per-line bet.<br>
-	Bet on more lines to get wins from secondary paylines.<br>
-	A sequence is a series of symbols next to each other on a payline.<br>
-	Only the best winning sequence is considered for reward distribution.<br>
-	For instance, 2 WILD + 2 SCATTER symbols on a line will reward 10x the bet.<br>
-	* Free games using <img class="silver_ticket_logo" src="images/assets/silver_ticket.svg"> excluded.'
+  	'Las ganancias on multiplicadas por el monto de la apuesta.<br>
+	Apuesta a más líneas para obtener premios de líneas secundarias.<br>
+	Una secuencia es una serie de símbolos consecutivos a lo largo de una de las líneas del juego.<br>
+	Para la distribución del premio solo se considera la secuencia con mejor puntaje.<br>
+	Por ejemplo, 2 WILD + 2 SCATTER en una misma línea multiplicarán la apuesta por 10.<br>
+	* Los juegos gratuitos que emplean <img class="silver_ticket_logo" src="images/assets/silver_ticket.svg"> se encuentran excluidos.'
 );
 ?>
